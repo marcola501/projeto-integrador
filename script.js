da95023dd6c3e2ca99bdb1e208218b52ed3285db
marcola501 <marcos.augusto04demaio2009@gmail.com>
2025-10-17 12:09:13 -0300

    beta do site
    V0.1: foi construido os links anexados no topo que acompanha pagina, paragrafos, estilo etc foram deixados responsivos para aparelhos moveis

---
+++ script.js
@@ -0,0 +1,37 @@
+function alterarlayout(){
+    const largura = window.innerWidth;
+    const nav = document.querySelector('nav');
+    if (largura < 769) {
+        nav.style.fontSize = '0.6rem';
+        nav.style.flexDirection = 'column';
+        nav.style.height = 'auto';
+        nav.style.alignItems = 'center'; 
+    }
+    else{
+        nav.style.fontSize = '1rem';
+        nav.style.alignItems = 'space-between';
+        nav.style.flexDirection = 'row';
+        nav.style.height = '10vh';
+    }
+    const paragrafos = document.querySelectorAll('.conteudo');
+    if (largura < 769) {
+        paragrafos.forEach(p => {
+            p.style.flexDirection = 'column';
+        });
+    }
+    else{
+        paragrafos.forEach(p => {
+            p.style.flexDirection = 'row';
+        });
+    }
+    const sobre = document.getElementById('sobre');
+    if (largura < 769) {
+        sobre.style.marginTop = '40vw';
+    }
+    else{
+        sobre.style.marginTop = '5%';
+    }
+};
+
+window.addEventListener('resize', alterarlayout);
+window.addEventListener('load', alterarlayout);
