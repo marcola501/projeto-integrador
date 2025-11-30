1b807a666ec14bfa780c6b5565e0a0d5cdbf6a64
marcola501 <marcos.augusto04demaio2009@gmail.com>
2025-11-30 18:23:09 -0300

    backup antes da grande atualização

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function alterarlayout(){
-    const largura = window.innerWidth;
-    const nav = document.querySelector('nav');
-    if (largura < 769) {
-        nav.style.fontSize = '0.6rem';
-        nav.style.flexDirection = 'column';
-        nav.style.height = 'auto';
-        nav.style.alignItems = 'left'; 
-        nav.style.justifyContent = 'left';
+    const largura = document.documentElement.clientWidth;//pega a largura da tela
+    const nav = document.querySelector('nav')//pega o elemento nav
+    if (largura < 790) {//se a largura for menor que 769px
+        nav.style.fontSize = '0.7rem';//altera o tamanho da fonte
+        nav.style.flexDirection = 'column';//altera a direção do flexbox
+        nav.style.height = 'auto';//altera a altura do nav
+        nav.style.alignItems = 'left'; //altera o alinhamento dos itens
+        nav.style.justifyContent = 'left';//altera a justificação dos itens
     }
     else{
         nav.style.fontSize = '1rem';
@@ -16,7 +16,7 @@ function alterarlayout(){
         nav.style.justifyContent = 'space-evenly';
     }
     const paragrafos = document.querySelectorAll('.conteudo');
-    if (largura < 769) {
+    if (largura < 790) {//769px
         paragrafos.forEach(p => {
             p.style.flexDirection = 'column';
         });
@@ -27,7 +27,7 @@ function alterarlayout(){
         });
     }
     const sobre = document.getElementById('sobre');
-    if (largura < 769) {
+    if (largura < 790) {
         sobre.style.marginTop = '40vw';
     }
     else{
