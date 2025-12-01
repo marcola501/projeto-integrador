3bc24d0f479630f7533ae18327f00cb90b937c22
marcola501 <marcos.augusto04demaio2009@gmail.com>
2025-11-30 22:31:47 -0300

    beta do grande update

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 function alterarlayout(){
     const largura = document.documentElement.clientWidth;//pega a largura da tela
     const nav = document.querySelector('nav')//pega o elemento nav
-    if (largura < 790) {//se a largura for menor que 769px
+    if (largura < 769) {//se a largura for menor que 769px
         nav.style.fontSize = '0.7rem';//altera o tamanho da fonte
         nav.style.flexDirection = 'column';//altera a direção do flexbox
         nav.style.height = 'auto';//altera a altura do nav
         nav.style.alignItems = 'left'; //altera o alinhamento dos itens
         nav.style.justifyContent = 'left';//altera a justificação dos itens
     }
-    else{
-        nav.style.fontSize = '1rem';
-        nav.style.alignItems = 'space-between';
+    else if(largura < 1025) {//telas de tablet
+        nav.style.fontSize = '0.8rem';//altera o tamanho da fonte
+        nav.style.alignItems = 'center';
         nav.style.flexDirection = 'row';
-        nav.style.height = '10vh';
-        nav.style.justifyContent = 'space-evenly';
+        nav.style.height = '8vh';//altera a altura do nav
+        nav.style.justifyContent = 'space-evenly';//altera a justificação dos itens
+    }
+    else{//telas de notebook e desktop
+        nav.style.fontSize = '1rem';//altera o tamanho da fonte
+        nav.style.alignItems = 'space-between';//altera o alinhamento dos itens
+        nav.style.flexDirection = 'row';//altera a direção do flexbox
+        nav.style.height = '9vh';//altera a altura do nav
+        nav.style.justifyContent = 'space-evenly';//altera a justificação dos itens
     }
     const paragrafos = document.querySelectorAll('.conteudo');
-    if (largura < 790) {//769px
+    if (largura < 769) {//769px
         paragrafos.forEach(p => {
-            p.style.flexDirection = 'column';
+            p.style.flexDirection = 'column';//altera a direção do flexbox
         });
     }
     else{
@@ -27,7 +34,7 @@ function alterarlayout(){
         });
     }
     const sobre = document.getElementById('sobre');
-    if (largura < 790) {
+    if (largura < 769) {
         sobre.style.marginTop = '40vw';
     }
     else{
